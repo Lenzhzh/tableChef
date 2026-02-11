e4e90f0b6ad3a5ed9f0e043314c439b9cd3b8f90
lenzhzh <q2425985947q@163.com>
2026-02-11 11:12:22 +0800

    fixed

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: './tableChef/', // Use relative paths for assets to work on GH Pages without custom domain
-})
+  base: '/tableChef/',
+  build: {
+    outDir: 'dist',
+  }
+});
